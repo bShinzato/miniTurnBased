41c0ab7a757d38be9db4f5f73f117a72ff86c8b0
Brandon Shinzato <131624991+bShinzato@users.noreply.github.com>
2026-04-09 23:55:38 -0500

    added modal for shop, with logic for coins and the shop with updated attack pattern

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -4,7 +4,11 @@ import { useState } from "react";
 export default function GamePage() {
   const [playerName, setPlayerName] = useState("");
   const [playerNameInput, setPlayerNameInput] = useState("");
+  const [playerCoins, setPlayerCoins] = useState(0);
   const [gameStep, setGameStep] = useState("nameRender");
+
+  const [showPotionModal, setShowPotionModal] = useState(false);
+  const [showMessage, setShowMessage] = useState("");
 
   const [playerHp, setPlayerHp] = useState(30);
   const [enemyName, setEnemyName] = useState("");
@@ -14,6 +18,20 @@ export default function GamePage() {
     const arrNames = ["Troll", "Gremlin", "Chunkie", "Doise"];
     const randomIndex = Math.floor(Math.random() * arrNames.length);
     return arrNames[randomIndex];
+  }
+
+  function coinsToNames(): number {
+    if (enemyName === "Troll") {
+      return 5;
+    } else if (enemyName === "Gremlin") {
+      return 3;
+    } else if (enemyName === "Chunkie") {
+      return 2;
+    } else if (enemyName === "Doise") {
+      return 1;
+    } else {
+      return 0;
+    }
   }
 
   function randomEnemyHp(): number {
@@ -28,24 +46,26 @@ export default function GamePage() {
     return Math.floor(Math.random() * 5) + 1;
   }
 
-  function attackTarget() {
-    const damage = randomPlayerAttackNum();
-    const newEnemyHp = enemyHp - damage;
-    if (newEnemyHp < 0) {
-      setEnemyHp(0);
-    } else {
-      setEnemyHp(newEnemyHp);
-    }
-  }
-
   function attackPlayer() {
     const damage = randomEnemyAttackNum();
     const newPlayerHp = playerHp - damage;
-    if (newPlayerHp < 0) {
+    if (newPlayerHp <= 0) {
       setPlayerHp(0);
       setGameStep("GameOver");
     } else {
       setPlayerHp(newPlayerHp);
+    }
+  }
+
+  function attackTarget() {
+    const damage = randomPlayerAttackNum();
+    const newEnemyHp = enemyHp - damage;
+    if (newEnemyHp <= 0) {
+      setEnemyHp(0);
+      setPlayerCoins((prevCoins): number => prevCoins + coinsToNames());
+    } else {
+      setEnemyHp(newEnemyHp);
+      attackPlayer();
     }
   }
 
@@ -58,7 +78,9 @@ export default function GamePage() {
   if (gameStep === "nameRender") {
     return (
       <main className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-xl font-bold mb-2 p-2  animate-pulse">Enter your Heroes Name</h1>
+        <h1 className="text-xl font-bold mb-2 p-2  animate-pulse">
+          Enter your Heroes Name
+        </h1>
         <input
           type="text"
           value={playerNameInput}
@@ -87,9 +109,15 @@ export default function GamePage() {
   if (gameStep === "Town") {
     return (
       <main className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-xl font-bold mb-2 p-2  animate-pulse">Welcome to Town, {playerName}.</h1>
+        <h1 className="text-xl font-bold mb-2 p-2  animate-pulse">
+          Welcome to Town, {playerName}.
+        </h1>
+        <p>
+          <li>You have {playerHp} Hit Points</li>
+          <li>{playerCoins} coins available</li>
+        </p>
         <button
-        className="mt-6 bg-blue-500 text-white px-5 py-3 rounded hover:bg-blue-600 hover:scale-105 hover:animate-bounce transition-duration-5000 hover:cursor-pointer"
+          className="mt-6 bg-blue-500 text-white px-5 py-3 rounded hover:bg-blue-600 hover:scale-105 hover:animate-bounce transition-duration-5000 hover:cursor-pointer"
           onClick={() => {
             setGameStep("Battle");
             setEnemyName(randomName());
@@ -98,6 +126,51 @@ export default function GamePage() {
         >
           Forest
         </button>
+        <div>
+          <button
+            className="mt-6 bg-blue-500 text-white px-5 py-3 rounded hover:bg-blue-600 hover:scale-105 hover:animate-bounce transition-duration-5000 hover:cursor-pointer"
+            onClick={() => {
+              setShowPotionModal(true);
+            }}
+          >
+            Buy Potion
+          </button>
+        </div>
+        {showPotionModal === true && (
+          <div className="fixed inset-0 bg-black/50 flex items-center justify-center">
+            <div className="bg-blue-400 p-6 rounded-lg shadow-lg">
+              <p>Buy Potion for 5 coins to restore 10 HP?</p>
+              {showMessage && <p>{showMessage}</p>}
+              <div className="flex justify-center">
+                <button
+                  onClick={() => {
+                    if (playerCoins >= 5) {
+                      setPlayerCoins((prev) => prev - 5);
+                      setPlayerHp((prev) => prev + 10);
+                      setShowMessage(`Purchased Completed.`);
+                    } else {
+                      setShowMessage(
+                        `You Broke NOOB! You only have ${playerCoins} coins!`,
+                      );
+                    }
+                  }}
+                  className="mr-2 mt-2 bg-blue-500 text-white px-4 py-1 rounded hover:bg-blue-600 hover:scale-105 transition-duration-5000 hover:cursor-pointer ring-1"
+                >
+                  Yes
+                </button>
+                <button
+                  onClick={() => {
+                    setShowPotionModal(false);
+                    setShowMessage("")
+                  }}
+                  className="ml-2 mt-2 bg-red-500 text-white px-4 py-1 rounded hover:bg-red-600 hover:scale-105 transition-duration-5000 hover:cursor-pointer ring-1"
+                >
+                  Exit
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
       </main>
     );
   }
@@ -108,15 +181,14 @@ export default function GamePage() {
         <h1 className="text-xl font-bold mb-2 p-2  animate-pulse">
           {enemyHp > 0
             ? `${enemyName} has appear. ${enemyName} has ${enemyHp} Hit Points. ${playerName} has ${playerHp} Hit Points.`
-            : `${enemyName} has 0 Hit Points. ${playerName} has slain ${enemyName}.`}
+            : `${enemyName} has 0 Hit Points. ${playerName} has slain ${enemyName} and received ${coinsToNames()} coins. ${playerName} has ${playerCoins} coins!`}
         </h1>
 
         {enemyHp > 0 ? (
           <button
-          className="mt-6 bg-blue-500 text-white px-5 py-3 rounded hover:bg-blue-600 hover:scale-105 hover:animate-bounce transition-duration-5000 hover:cursor-pointer"
+            className="mt-6 bg-blue-500 text-white px-5 py-3 rounded hover:bg-blue-600 hover:scale-105 hover:animate-bounce transition-duration-5000 hover:cursor-pointer"
             onClick={() => {
               attackTarget();
-              attackPlayer();
             }}
           >
             Attack!
@@ -124,16 +196,15 @@ export default function GamePage() {
         ) : (
           <>
             <button
-            className="mt-6 bg-blue-500 text-white px-5 py-3 rounded hover:bg-blue-600 hover:scale-105 hover:animate-bounce transition-duration-5000 hover:cursor-pointer"
+              className="mt-6 bg-blue-500 text-white px-5 py-3 rounded hover:bg-blue-600 hover:scale-105 hover:animate-bounce transition-duration-5000 hover:cursor-pointer"
               onClick={() => {
                 setGameStep("Town");
-                setPlayerHp(30);
               }}
             >
-              Return to Town to heal up.
+              Return to Town.
             </button>
             <button
-            className="mt-6 bg-blue-500 text-white px-5 py-3 rounded hover:bg-blue-600 hover:scale-105 hover:animate-bounce transition-duration-5000 hover:cursor-pointer"
+              className="mt-6 bg-blue-500 text-white px-5 py-3 rounded hover:bg-blue-600 hover:scale-105 hover:animate-bounce transition-duration-5000 hover:cursor-pointer"
               onClick={() => {
                 setGameStep("Battle");
                 setEnemyName(randomName());
@@ -151,12 +222,15 @@ export default function GamePage() {
   if (gameStep === "GameOver") {
     return (
       <main className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-xl font-bold mb-2 p-2  animate-pulse">Game Over, {playerName}.</h1>
+        <h1 className="text-xl font-bold mb-2 p-2  animate-pulse">
+          Game Over, {playerName}.
+        </h1>
         <button
-        className="mt-6 bg-blue-500 text-white px-5 py-3 rounded hover:bg-blue-600 hover:scale-105 hover:animate-bounce transition-duration-5000 hover:cursor-pointer"
+          className="mt-6 bg-blue-500 text-white px-5 py-3 rounded hover:bg-blue-600 hover:scale-105 hover:animate-bounce transition-duration-5000 hover:cursor-pointer"
           onClick={() => {
             setGameStep("nameRender");
             setPlayerHp(30);
+            setPlayerCoins(0);
           }}
         >
           Play Again
